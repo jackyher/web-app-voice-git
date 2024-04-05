4437ee2c0d16f3e8e31fe0d6c7f56abeebbc0b91
jackyher <jackyhernandez0702@gmail.com>
2024-04-04 23:58:35 -0600

    Envío de peticiones

--- script.js
+++ script.js
@@ -16,58 +16,11 @@ document.addEventListener('DOMContentLoaded', function () {
         //Evento cuando la voz es detectada
         recognition.onresult = function (event) {
             const transcript = event.results[0][0].transcript;
-            const keyword = 'tamaño 3';
-            const abrirPestaña = 'abrir';
-            const irPagina = 'canva.com';
-            const tamañoVentana = 'tamaño de la ventana';
-            const cerrarPestaña = 'cerrar';
-            const aumentarZoom = 'aumentar';
-            const reducirZoom = 'reducir';
 
             resultDiv.innerHTML = `<p>Orden identificada: <strong>${transcript}</strong></p>`;
 
-            //Para cambiar tamaño del texto
-            if(transcript.includes(keyword)) { //Para encontrar palabra en específico
-                controlTexto.classList.add('fs-3');
-                console.log("Sí se encontró la palabra...");
-            }
-
-            //Para abrir una nueva pestaña en el navegador
-            if(transcript.includes(abrirPestaña)) {
-                window.open("https://www.google.com/" , "_blank");
-                console.log("Nueva pestaña del navegador abierta");
-            }
-
-            //Para visitar la página de canva
-            if(transcript.includes(irPagina)) {
-                window.open("https://www.canva.com" , "_blank");
-                console.log("Se ha visitado Canva");
-            }
-
-            //Para modificar el tamaño de la ventana del navegador
-            if(transcript.includes(tamañoVentana)) {
-                //window.resizeTo(800,600);
-                window.open("https://jackyher.github.io/web-app-voice-git/", "_blank", "width=800,height=600");
-                console.log("El tamaño de la ventana del navegador ha cambiado a 800, 600");
-            }
-
-            //Para cerrar la pestaña del navegador actual
-            if(transcript.includes(cerrarPestaña)) {
-                window.close();
-                console.log("Todas las pestañas del navegador se han cerrado");
-            }
-
-            //Para aumentar el zoom de la ventana
-            if(transcript.includes(aumentarZoom)) {
-                document.body.style.zoom = "150%";
-                console.log("Zoom aumentado al 150%");
-            }
-
-            //Para restablecer el zoom de la ventana
-            if(transcript.includes(reducirZoom)) {
-                document.body.style.zoom = "100%";
-                console.log("Zoom aumentado al 100%");
-            }
+            //Ejecutar acciones correspondientes
+            executeActions(transcript);
         };
 
         // Evento de error
@@ -76,5 +29,83 @@ document.addEventListener('DOMContentLoaded', function () {
         };
 
         recognition.start();
+    }
+
+    //Función para ejecutar las acciones correspondientes
+    function executeActions(transcript) {
+        const keyword = 'tamaño 3';
+        const abrirPestaña = 'abrir';
+        const irPagina = 'canva.com';
+        const tamañoVentana = 'tamaño de la ventana';
+        const cerrarPestaña = 'cerrar';
+        const aumentarZoom = 'aumentar';
+        const reducirZoom = 'reducir';
+        
+        let enviarMockapi = false;
+
+        const fecha = new Date();
+        const formatFecha = fecha.toLocaleString(); //Para convertir la fecha a la zona horaria del usuario
+
+        //Para cambiar tamaño del texto
+        if(transcript.includes(keyword)) { //Para encontrar palabra en específico
+            controlTexto.classList.add('fs-3');
+            console.log("Sí se encontró la palabra...");
+            enviarMockapi = true;
+        }
+
+        //Para abrir una nueva pestaña en el navegador
+        if(transcript.includes(abrirPestaña)) {
+            window.open("https://www.google.com/" , "_blank");
+            console.log("Nueva pestaña del navegador abierta");
+            enviarMockapi = true;
+        }
+
+        //Para visitar la página de canva
+        if(transcript.includes(irPagina)) {
+            window.open("https://www.canva.com" , "_blank");
+            console.log("Se ha visitado Canva");
+            enviarMockapi = true;
+        }
+
+        //Para modificar el tamaño de la ventana del navegador
+        if(transcript.includes(tamañoVentana)) {
+            //window.resizeTo(800,600);
+            window.open("https://jackyher.github.io/web-app-voice-git/", "_blank", "width=800,height=600");
+            console.log("El tamaño de la ventana del navegador ha cambiado a 800, 600");
+            enviarMockapi = true;
+        }
+
+        //Para cerrar la pestaña del navegador actual
+        if(transcript.includes(cerrarPestaña)) {
+            window.close();
+            console.log("Todas las pestañas del navegador se han cerrado");
+            enviarMockapi = true;
+        }
+
+        //Para aumentar el zoom de la ventana
+        if(transcript.includes(aumentarZoom)) {
+            document.body.style.zoom = "150%";
+            console.log("Zoom aumentado al 150%");
+            enviarMockapi = true;
+        }
+
+        //Para restablecer el zoom de la ventana
+        if(transcript.includes(reducirZoom)) {
+            document.body.style.zoom = "100%";
+            console.log("Zoom aumentado al 100%");
+            enviarMockapi = true;
+        }
+
+        //Para enviar a Mockapi
+        if(enviarMockapi) {
+            const orden = { orden: transcript, fecha: formatFecha }; //Agregamos datos al objeto
+            axios.post('https://660f6f5a356b87a55c51616a.mockapi.io/orden', orden)
+            .then(response => {
+                console.log("Orden enviada: ", response.data);
+            })
+            .catch(error => {
+                console.error("Error al enviar la orden: ", error);
+            });
+        }
     }
 });
